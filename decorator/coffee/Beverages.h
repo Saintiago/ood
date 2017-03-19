31b7185999d86cbd53c2b05e0606b91f628f0b4b
Saintiago <techpriest2007@yandex.ru>
2017-03-19 21:08:05 +0300

    Треетья лабораторная. Пример в котором буду делать вторую лабораторную.

---
+++ decorator/coffee/Beverages.h
@@ -0,0 +1,169 @@
+﻿#pragma once
+
+#include "IBeverage.h"
+
+class CBeverage : public IBeverage
+{
+public:
+	CBeverage(double cost, std::string description)
+		: m_cost(cost)
+		, m_description(description)
+	{}
+
+	std::string GetDescription() const override
+	{
+		return m_description;
+	}
+
+	double GetCost() const override
+	{
+		return m_cost;
+	}
+
+private:
+	double m_cost;
+	std::string m_description;
+};
+
+// Кофе
+class CCoffee : public CBeverage
+{
+public:
+	CCoffee(double cost = 60, std::string description = "Coffee") : CBeverage(cost, description) {}
+};
+
+enum class CapuccinoType
+{
+	Single,
+	Double
+};
+
+// Капуччино
+class CCapuccino : public CCoffee
+{
+public:
+	CCapuccino(CapuccinoType capuccinoType)
+		: CCoffee(determineCost(capuccinoType), determineDescription(capuccinoType))
+	{}
+
+private:
+	double determineCost(CapuccinoType capuccinoType)
+	{ 
+		return (capuccinoType == CapuccinoType::Single) ? 80 : 120;
+	}
+	std::string determineDescription(CapuccinoType capuccinoType)
+	{ 
+		return (capuccinoType == CapuccinoType::Single) ? "Single capuccino" : "Double capuccino";
+	}
+};
+
+enum class LatteType
+{
+	Single,
+	Double
+};
+
+// Латте
+class CLatte : public CCoffee
+{
+public:
+	CLatte(LatteType latteType)
+		: CCoffee(determineCost(latteType), determineDescription(latteType))
+	{}
+
+private:
+	double determineCost(LatteType latteType)
+	{
+		return (latteType == LatteType::Single) ? 90 : 130;
+	}
+	std::string determineDescription(LatteType latteType)
+	{
+		return (latteType == LatteType::Single) ? "Single Latte" : "Double Latte";
+	}
+};
+
+enum class TeaType
+{
+	Nuri,
+	Maiskiy,
+	So_slonom,
+	Lipton
+};
+
+// Чай
+class CTea : public CBeverage
+{
+public:
+	CTea(TeaType teaType)
+		: CBeverage(30, determineDescription(teaType))
+	{}
+
+private:
+
+	std::string determineDescription(TeaType teaType)
+	{
+		switch(teaType)
+		{
+			case TeaType::Nuri:
+				return "Nuri tea";
+				break;
+			case TeaType::Maiskiy:
+				return "Maiskiy tea";
+				break;
+			case TeaType::So_slonom:
+				return "So slonom tea";
+				break;
+			case TeaType::Lipton:
+				return "Lipton tea";
+		}
+	}
+
+};
+
+enum class MilkshakeType
+{
+	Small,
+	Medium,
+	Large
+};
+
+// Молочный коктейль
+class CMilkshake : public CBeverage
+{
+public:
+	CMilkshake(MilkshakeType milkshakeType)
+		: CBeverage(determineCost(milkshakeType), determineDescription(milkshakeType))
+	{}
+	
+private:
+
+	double determineCost(MilkshakeType milkshakeType)
+	{
+		switch (milkshakeType)
+		{
+		case MilkshakeType::Small:
+			return 50;
+			break;
+		case MilkshakeType::Medium:
+			return 60;
+			break;
+		case MilkshakeType::Large:
+			return 80;
+		}
+	}
+
+	std::string determineDescription(MilkshakeType milkshakeType)
+	{
+		switch (milkshakeType)
+		{
+		case MilkshakeType::Small:
+			return "Small milkshake";
+			break;
+		case MilkshakeType::Medium:
+			return "Medium milkshake";
+			break;
+		case MilkshakeType::Large:
+			return "Large milkshake";
+		}
+	}
+};
---
+++ decorator/coffee/main.cpp
@@ -0,0 +1,131 @@
+﻿#include "Beverages.h"
+#include "Condiments.h"
+
+#include <iostream>
+#include <string>
+#include <functional>
+
+using namespace std;
+
+
+/*
+Функциональный объект, создающий лимонную добавку
+*/
+struct MakeLemon
+{
+	MakeLemon(unsigned quantity)
+		:m_quantity(quantity)
+	{}
+
+	auto operator()(IBeveragePtr && beverage)const
+	{
+		return make_unique<CLemon>(move(beverage), m_quantity); 
+	}
+private:
+	unsigned m_quantity;
+};
+
+/*
+Функция, возвращающая функцию, создающую коричную добавку
+*/
+function<IBeveragePtr(IBeveragePtr &&)> MakeCinnamon()
+{
+	return [] (IBeveragePtr && b) {
+		return make_unique<CCinnamon>(move(b));
+	};
+}
+
+/*
+Возвращает функцию, декорирующую напиток определенной добавкой
+
+Параметры шаблона: 
+	Condiment - класс добавки, конструктор которого в качестве первого аргумента
+				принимает IBeveragePtr&& оборачиваемого напитка
+	Args - список типов прочих параметров конструктора (возможно, пустой)
+*/
+template <typename Condiment, typename... Args>
+auto MakeCondiment(const Args&...args)
+{
+	// Возвращаем функцию, декорирующую напиток, переданный ей в качестве аргумента
+	// Дополнительные аргументы декоратора, захваченные лямбда-функцией, передаются
+	// конструктору декоратора через make_unique
+	return [=](auto && b) {
+		// Функции make_unique передаем b вместе со списком аргументов внешней функции
+		return make_unique<Condiment>(forward<decltype(b)>(b), args...);
+	};
+}
+
+/*
+Перегруженная версия оператора <<, которая предоставляет нам синтаксический сахар
+для декорирования компонента
+
+Позволяет создать цепочку оборачивающих напиток декораторов следующим образом:
+auto beverage = make_unique<CConcreteBeverage>(a, b, c)
+					<< MakeCondimentA(d, e, f)
+					<< MakeCondimentB(g, h);
+
+Функциональные объекты MakeCondiment* запоминают аргументы, необходимые для создания
+дополнения, и возвращают фабричную функцию, принимающую оборачиваемый напиток, которая
+при своем вызове создаст нужный объект Condiment, передав ему запомненные аргументы.
+Использование:
+	auto beverage = 
+		make_unique<CConcreteBeverage>(a, b, c)
+		<< MakeCondimentA(d, e, f)
+		<< MakeCondimentB(g, h);
+или даже так:
+	auto beverage = 
+		make_unique<CConcreteBeverage>
+		<< MakeCondiment<CondimentA>(d, e, f)
+		<< MakeCondiment<CondimentB>(g, h);
+В последнем случае нет необходимости писать вручную реализации MakeCondimentA и MakeCondimentB, т.к.
+необходимую реализацию сгенерирует компилятор
+
+Классический способ оборачивания выглядел бы так:
+	auto baseBeverage = make_unique<CConcretedBeverage>(a, b, c);
+	auto wrappedWithCondimentA = make_unique<CCondimentA>(move(baseBeverage), d, e, f);
+	auto beverage = make_unique<CCondimentB>(move(wrappedWithCondimentA), g, h);
+либо так:
+	auto beverage = make_unique<CCondimentB>(
+						make_unique<CCondimentA>(
+							make_unique<CConcreteBeverage>(a, b, c), // Напиток
+							d, e, f	// доп. параметы CondimentA
+						),
+						g, h		// доп. параметры CondimentB
+					);
+
+unique_ptr<CLemon> operator << (IBeveragePtr && lhs, const MakeLemon & factory)
+{
+	return factory(move(lhs));
+}
+unique_ptr<CCinnamon> operator << (IBeveragePtr && lhs, const MakeCinnamon & factory)
+{
+	return factory(move(lhs));
+}
+*/
+template <typename Component, typename Decorator>
+auto operator << (Component && component, const Decorator & decorate)
+{
+	return decorate(forward<Component>(component));
+}
+
+
+int main()
+{
+	{
+		// Наливаем чашечку латте
+		auto latte = make_unique<CTea>(TeaType::Nuri);
+		// добавляем корицы
+		auto cinnamon = make_unique<CCinnamon>(move(latte));
+		// добавляем пару долек лимона
+		auto liquor = make_unique<CLiquor>(move(cinnamon), LiquorType::Walnut);
+		// добавляем пару кубиков льда
+		auto chocolate = make_unique<CChocolateSlice>(move(liquor), 6);
+		// добавляем сливки
+		auto cream = make_unique<CCream>(move(chocolate));
+		// добавляем 2 грамма шоколадной крошки
+		auto beverage = make_unique<CChocolateCrumbs>(move(cream), 2);
+
+		// Выписываем счет покупателю
+		cout << beverage->GetDescription() << " costs " << beverage->GetCost() << endl;
+	}
+}
